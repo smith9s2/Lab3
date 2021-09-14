410141dd93e319e353e3561442113257722837a6
smith9s2 <89741567+smith9s2@users.noreply.github.com>
2021-09-14 16:09:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    int number;
+
+    System.out.println("Enter a number to count down or up from: ");
+    number = s.nextInt();
+
+    if (number>0) {
+      while (number>=0) {
+        System.out.println(number);
+        number -= 1;
+      }
+    }
+    else while (number<=0) {
+      System.out.println(number);
+      number += 1;
+    }
+
+    System.out.println("Blast Off!!");
+    
+    
+    
+    
+    
+    /*while (number >=0) 
+    {
+      System.out.println(number);
+      number -= 1;
+    }
+    else if (number <=0)
+    {
+      System.out.println(number);
+      number += 1;
+    }*/
+    }
+  }
